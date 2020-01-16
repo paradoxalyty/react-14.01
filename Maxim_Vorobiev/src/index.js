aad73f96420982a38c9c4cfb5ca79714a63425f7
Maxim <maxpostal@mail.ru>
2020-01-16 12:30:31 +0300

    Tasks #1, 2, and 3

---
+++ Maxim_Vorobiev/src/index.js
@@ -0,0 +1,19 @@
+import {script} from "./second-index";
+
+// second-script.js
+script();
+
+// script.js
+// Test Babel
+export const ES6 = () => {
+    console.log('ES6');
+};
+
+ES6();
+
+
+// React
+import React from "react";
+import ReactDom from "react-dom";
+
+const root = document.getElementById('root');
---
+++ Maxim_Vorobiev/src/second-index.js
@@ -0,0 +1,3 @@
+export function script() {
+    console.warn('Test warning');
+}
