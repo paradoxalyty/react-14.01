5cd9d04ede5aab5ebdabbb6a23d13791aa1989e0
slon <veremchukvv@gmail.com>
2020-01-16 23:59:32 +0300

    добавил кнопку и отправку тестового сообщения

--- vladimir.veremchuk/static_src/index.js
+++ vladimir.veremchuk/static_src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const messages = [
+let messages = [
     {name: "Ivan", content: "Hello!"},
     {name: "Oleg", content: "Hi, how are you?"},
     {name: "Ivan", content: "I am well"}
@@ -12,5 +12,13 @@ const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</di
 function MessageList({messages}) {
     return messages.map((message, index) => <Message {...message} key={index}/>)
 }
+
+function sendMessage() {
+    messages.push({name: "ChatBot", content: "test"});
+    ReactDom.render(<MessageList messages={messages}/>, document.getElementById("root"));
+    console.log(messages);
+}
+
+document.getElementById('send').addEventListener('click', sendMessage);
 
 ReactDom.render(<MessageList messages={messages}/>, document.getElementById("root"));
