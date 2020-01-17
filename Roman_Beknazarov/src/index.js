8c03225dbbffbec1b2d6f29d9fa17f723745615c
paradoxalyty <paradoxalyty@gmail.com>
2020-01-17 01:42:19 +0100

    installed @babel...
    JSX - compiled successfuly

--- Roman_Beknazarov/src/index.js
+++ Roman_Beknazarov/src/index.js
@@ -1,1 +1,14 @@
-console.log("console");
+import React from "react";
+import ReactDom from "react-dom";
+
+
+
+const Link = ({title, link}) => <a href={"https://" + link}>{title}</a>; // JSX
+
+ReactDom.render(<Link title="This is a link" link="google.ru" />, document.getElementById("root"));
+
+/*
+// without JSX
+return React.createElement("a", {"href": "https://" + props.link}, props.title);
+ReactDom.render(React.createElement(Link, {title: "This is a link", link: "google.ru"}), document.getElementById("root"));
+*/
