00ec2d0c20811f12adc04593c25675d35665557a
katerina <katerina_sanway@mail.ru>
2020-01-17 14:45:03 +0300

    Создание и настройка проекта

---
+++ Ekaterina_Kirilenko/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const messages = [
+  {name: "Katya", content: "Hello!"},
+  {name: "Maksim", content: "Hi!"},
+  {name: "Katya", content: "How are you?"},
+];
+
+const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>;
+
+const MessageList = ({messages}) => {
+  return messages.map((message, index) => (
+    <Message {...message} key={index} /> //если св-ва одинаковые
+    // <Message name={message.name} content={message.content} key={index} />
+  ));
+}
+
+ReactDom.render(
+  <MessageList messages={messages} />,
+  document.getElementById("root")
+);
+
+//<Link title="Привет! Это ссылка" link="google.com" />
+//const Link = ({ title, link }) => <a href={"https://" + link}>{title}</a>;
+//const element = <h1 className="element">Мы подключили React</h1>;
