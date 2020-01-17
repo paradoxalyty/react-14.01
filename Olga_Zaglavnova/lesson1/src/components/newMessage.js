9744d96f7c1d1940da8fcc9a8a582e24816df284
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2020-01-17 15:13:02 +0300

    Webpack собирает проект успешно. Вернула до более-менее рабочего варианта. Пока не отправляется новое сообщение, только выводится на консоль.

--- Olga_Zaglavnova/lesson1/src/components/newMessage.js
+++ Olga_Zaglavnova/lesson1/src/components/newMessage.js
@@ -9,21 +9,30 @@ export default class NewMessage extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleChange(event) {
         this.setState({value: event.target.value});
     }
     handleSubmit(event){
         let newMsjObj = {name: "Guest", content: this.state.value};
-        this.props.updateData(newMsjObj);
+        console.log('newMessage.js handleSubmit IS WORKING:', newMsjObj);
+        //this.props.updateData(newMsjObj);
         this.setState({value: ""});
 
         event.preventDefault();
     }
     render() {
         return <form onSubmit={this.handleSubmit} className="newMessageForm">
+            <label>
+            <strong>Я: </strong>
             <input className="messageInput" value={this.state.value} onChange={this.handleChange}></input>
+            </label>
             <input type="submit" value="Отправить" className="submitBtn" />
             </form>
         //return <div>Hello</div>
     }
+    /*render() {
+        return <form className="newMessageForm"><input className="messageInput" value={this.state.value} onChange={this.handleChange} /><input type="submit" value="Отправить" className="submitBtn" /></form>
+        //return <div>Hello</div>
+    }*/
 }
--- Olga_Zaglavnova/lesson1/src/index.js
+++ Olga_Zaglavnova/lesson1/src/index.js
@@ -4,24 +4,41 @@ import MessageList from './components/messageList';
 import NewMessage from "./components/newMessage";
 import css from 'style.css';
 
-class ChatPage extends React.Component {
-    constructor(props){
+const messages = [{name:"Робот", content: "Привет. Человек!"},
+    {name: "Я", content: "Привет, Робот!"},
+    {name:"Робот", content: "Как дела?"},
+    {name: "Я", content: "Да неплохо, идут..."},
+    {name:"Робот", content: "Рад за тебя!"}
+    ];
+
+/*class ChatPage extends React.Component {
+   /* constructor(props){
         super(props);
         this.state = {messages: []};
 
-        this.addNewMessage = this.addNewMessage.bind(this);
+        //this.updateData = this.updateData.bind(this);
+        //this.addNewMessage = this.addNewMessage.bind(this);
     };
-    updateData(value){
+    /*updateData(value){
         console.log('index.js updateData IS WORKING', value);
-    };
+    };*/
     /*addNewMessage(newMsg){
         this.messages.push(newMsg);
         console.log('index.js addNewMessage IS WORKING', this.messages);
 
     };*/
-    render(){
-        return <div className="container"><MessageList messages={this.messages} /><NewMessage updateData = {this.updateData.bind(this)} /></div>
+ /*   render(){
+        return <div className="container">
+            <MessageList messages={this.state.messages} />
+            <NewMessage updateData = {this.updateData} />
+        </div>
+    };*/
+    /*render(){
+       // return <div className="container"><MessageList messages={this.state.messages} /><NewMessage /></div>
+        return <div className="container"><MessageList messages={messages} /><NewMessage /></div>
     };
-}
+};*/
+
+    const ChatPage = <div className="container"><MessageList messages={messages} /><NewMessage /></div>
 
 ReactDom.render(ChatPage, document.getElementById("root"));
