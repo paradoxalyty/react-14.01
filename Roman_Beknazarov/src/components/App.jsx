2ccb6361de2bda7544334da047c3cf58060499a0
paradoxalyty <paradoxalyty@gmail.com>
2020-01-18 21:20:30 +0100

    props and propTypes

--- Roman_Beknazarov/src/components/App.jsx
+++ Roman_Beknazarov/src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Child from './Child';
 
 export default class App extends React.Component {
     state = {
-        text: 'My first React-component'
+        text: 'My first React-component',
+        counter: 0,
     };
 
     componentWillMount() {
@@ -11,19 +13,31 @@ export default class App extends React.Component {
 
     componentDidMount() {
         console.log("componentDidMount");
-        setTimeout(() => this.setState({
-            'text': 'refreshed React-component'
-        }), 1000);
     }
 
     componentDidUpdate() {
         console.log("componentDidUpdate");
     }
 
+    handleClick = () => {
+        this.setState({'counter': this.state.counter + 1})
+    };
+
     render() {
         console.log("render");
         return (
-            <h1>{this.state.text}</h1>
+            <div>
+                <h1>{this.state.text}</h1>
+                <Child counter={ this.state.counter } />
+                <button onClick={ this.handleClick }>+1</button>
+            </div>
         )
     }
 }
+
+
+/*
+
+setTimeout(() => this.setState({
+    'text': 'refreshed React-component'
+}), 1000);*/
--- Roman_Beknazarov/webpack.config.js
+++ Roman_Beknazarov/webpack.config.js
@@ -30,5 +30,9 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({template: path.resolve(__dirname, "src", "index.html"),})
-    ]
+    ],
+    resolve: {
+        modules: [`${__dirname}/static_src`, 'node_modules'],
+        extensions: ['.js', '.jsx'],
+    },
 };
