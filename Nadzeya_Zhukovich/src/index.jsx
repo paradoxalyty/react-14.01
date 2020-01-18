98965ddb0c087301878bfae98cd0ebc7a2608942
Nadzeya <naduva0505@gmail.com>
2020-01-18 15:18:10 +0100

    Added example from lesson

--- Nadzeya_Zhukovich/src/index.jsx
+++ Nadzeya_Zhukovich/src/index.jsx
@@ -1,3 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const Link = (props) =>  <a href={"http://" + props.link}>{props.title}</a>
+ReactDom.render(<Link title="This is a link" link="google.ru"></Link>, document.getElementById("root"));
+
+// return React.createElement("a", {"href": "https://" + props.href}, props.title);
+// React.createElement(Link, {title: "This is a link", link: "google.ru"}
