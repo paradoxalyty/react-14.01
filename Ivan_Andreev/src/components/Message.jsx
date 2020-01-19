6c2737d93949d4ce03117b94afe43e63d135526b
iasasl <lis75000@bk.ru>
2020-01-19 12:16:41 +0300

    Все задания решены

---
+++ Ivan_Andreev/src/components/Message.jsx
@@ -0,0 +1,3 @@
+import React, {Component} from 'react';
+
+export const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>
--- Ivan_Andreev/src/index.js
+++ Ivan_Andreev/src/index.js
@@ -1,15 +1,33 @@
 import ReactDom from 'react-dom';
 import React from 'react';
+import { MessageList } from './components/MessageList'
+
+const userName = "CurrentUser"
+const chatBotName = "ChatBot";
+const defaultChatBotAnswer = `Hey! Don't bother me, ${userName}`
 
 const messages = [
-    {name: "Ivan", content: "Hello!"},
-    {name: "NeIvan", content: "Hello2!"},
+    {name: chatBotName, content: `Hello! My name is ${chatBotName}`},
 ]
 
-const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>
+const Chat = (props) => {
+    const sendMessage = () => {
+        const sendMessageField = document.getElementById('send-message-field');
+        messages.push({name: userName, content: sendMessageField.value});
+        sendMessageField.value = "";
+        botAnswer();
+        ReactDom.render(<Chat messages={messages} />, document.getElementById('root'));
+    }
 
-const MessageList = ({messages}) => {
-    return messages.map((message, index) => <Message {...message} key={index}/>)
+    const botAnswer = () => {
+        messages.push({name: chatBotName, content: defaultChatBotAnswer})
+    }
+
+    return  <>
+                <MessageList messages={props.messages} />
+                <input type="text" placeholder="New message" id="send-message-field"/>
+                <input type="submit" value="New message" onClick={sendMessage}/>
+            </>
 }
 
-ReactDom.render(<MessageList messages={messages} />, document.getElementById('root'));
+ReactDom.render(<Chat messages={messages} />, document.getElementById('root'));
