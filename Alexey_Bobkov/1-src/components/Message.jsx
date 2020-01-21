89bc3ddab0cbfc6953859bc5e689b9fb0eaaf017
alexeybobkov47 <alexeybobkov47@gmail.com>
2020-01-21 15:42:15 +0400

    2ДЗ

--- Alexey_Bobkov/1-src/components/Message.jsx
+++ Alexey_Bobkov/1-src/components/Message.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Message extends React.Component{
+        /*
         static propTypes = {
             text: PropTypes.string.isRequired,
         };
+        */
 
         render () {
-            return <div>{ this.props.text }</div>
+            return <div>{this.props.name}: {this.props.content}</div>
         }
 }
--- Alexey_Bobkov/1-src/index.js
+++ Alexey_Bobkov/1-src/index.js
@@ -32,30 +32,4 @@ const MessageList = ({ messames }) => {
         }
 
         ReactDom.render( < MessageList > </MessageList>, document.getElementById('root'));
-
-
-
-                /*
-
-                ReactDom.render(<Link title="test link" link="google.com"></Link>,root);
-                ReactDom.render(<button onclick='' >Кнопка</button>,root);
-                function Link(props) {
-                    return <a href = { "https://" + props.link } > { props.title } </a>
-                }
-
-
-                function test(){
-                    return(
-                        <button onclick=''>Норм кнопка/>
-                        const Message=({name, content}) => <div><strong>{name}:</strong> {content}</div>;
-                        const MessageList = ({messames}) => {
-                        return messages.map((message, index)=> <Message {...message} key={index}/>)
-                }
-                        <MessageList></MessageList>
-                    )
-                }
-
-
-
-
                 */
