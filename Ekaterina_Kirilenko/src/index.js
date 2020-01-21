9121910b8b4619a5a203d3006eaf2e541f173cb1
katerina <katerina_sanway@mail.ru>
2020-01-21 13:18:36 +0300

    Созданы компоненты MessageField и Message, реализована функция отправки сообщения по кнопке, на сообщения отвечает робот

--- Ekaterina_Kirilenko/src/index.js
+++ Ekaterina_Kirilenko/src/index.js
@@ -1,32 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import MessageField from "./components/MessageField";
+// import { Counter } from "./test/Counter";
 
-const messages = [
-  {name: "Katya", content: "Hello!"},
-  {name: "Maksim", content: "Hi!"},
-  {name: "Katya", content: "How are you?"},
-];
-
-const Message = ({name, content}) => <div><strong>{name}:</strong> {content}</div>;
-
-const MessageList = ({messages}) => {
-  return messages.map((message, index) => (
-    <Message {...message} key={index} /> //если св-ва одинаковые
-    // <Message name={message.name} content={message.content} key={index} />
-  ));
-}
-
-const Button = <button>Отправить</button>;
-
-ReactDom.render(
-    [
-    <MessageList messages={messages} />, 
-    Button
-  ], 
-  document.getElementById("root")
-);
-
-
-// <Link title="Привет! Это ссылка" link="google.com" />
-// const Link = ({ title, link }) => <a href={"https://" + link}>{title}</a>;
-// const element = <h1 className="element">Мы подключили React</h1>;
+ReactDom.render(<MessageField />, document.getElementById("root"));
