65e5112ba685001e5e029122d5eeeb434256086c
annapuchkova <48117633+annapuchkova@users.noreply.github.com>
2020-01-24 18:33:14 +0400

    lesson 3

--- Anna_Puchkova/src/index.js
+++ Anna_Puchkova/src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-//import { messages } from './components/Message';
-import App from './components/MessageField';
-//import { Counter } from './components/Counter';
-//import { App } from './components/App';
-/*
-ReactDom.render(
-<MessageList messages={messages}/>, 
-document.getElementById("root")
-);
-//const Button = () => <button onClick={sendMessage}>Add Message</button>;
+import MessageField from './components/MessageField';
+import Layout from './components/Layout';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-export const Chat = (props) => {
-    return <>
-        <MessageList messages={messages}/>
-        <input type="submit" value="New message" onClick={sendMessage}/>
-    </>
-}*/
-
-ReactDom.render(
-    <App/>, 
+    ReactDom.render(
+        <Layout/>,      
     document.getElementById("root")
     );
 
