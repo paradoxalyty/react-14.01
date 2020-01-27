3f396eb78d8ad43628b60b8fa9186483f8011333
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-27 22:28:33 +0300

    Скопировал из папки Lesson2 себе в корень файлы проекта, теперь буду делать всё тут. Папки Lesson1 и Lesson2 удалил.

--- Vladislav Ovinkin/Lesson1/src/components/message.jsx
+++
@@ -1,4 +0,0 @@
-const Message = ({name, content}) => <div><strong>{ name }: </strong>{ content }</div>;
-// React.createElement ("div", {}, [React.createElement ("strong", {}, name), content]); // after babel conversion
-
-export { Message };
