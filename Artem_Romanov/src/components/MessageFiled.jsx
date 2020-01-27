53cf12329cfdff80fc3d859f457cef276d58130a
roartem <ro.artem@gmail.com>
2020-01-27 13:14:26 +0300

    Подготовил мессенджер к отправке сообщений

--- Artem_Romanov/src/components/MessageFiled.jsx
+++ Artem_Romanov/src/components/MessageFiled.jsx
@@ -19,21 +19,40 @@ componentDidUpdate() {
     }
 }
 //Отправка сообщений автора
-handleClick = () => {
-    this.setState({ messages: [ ...this.state.messages, {text: 'Отлично', sender: 'me'} ] });
-};
+// handleClick = () => {
+//     this.setState({ messages: [ ...this.state.messages, {text: 'Отлично', sender: 'me'} ] });
+// };
+// Передаем сообщение в качестве аргумента функции
+    handleClick = (message) => {
+        this.setState({ messages: [ ...this.state.messages, {text: message, sender: 'me'} ] });
+    };
+    //Регистрируем изменения в текстовом поле
+    handleChange = (event) => {
+        console.log(event.target.value);
+     };
+     //Обработчик отправки сообщений по enter
+     handleKeyUp = (event, message) => {
+         console.log(event.keyCode); //Тест кодов клавиш
+        if (event.keyCode === 13) { 
+            this.setState({ messages: [ ...this.state.messages, {text: message, sender: 'me'} ] });
+        }
+     };
 
 //Рендер и запрос класса из Message
 render() {
     const messageElements = this.state.messages.map((text, sender) =>      
      (<Message key={sender} text {...text}/>));
       
-
      return <div className="layout">
      <div className="message-field">
         { messageElements }
         </div>
-        <button onClick={ this.handleClick }>Отправить сообщение</button>
+        {/* Создаем текстовое поле для ввода сообщений и подключаем обработчик нажатия enteк */}
+        <input onChange={ this.handleChange } onKeyUp={ (event) => this.handleKeyUp(event, 'Нормально') } />
+        {/* <button onClick={ this.handleClick }>Отправить сообщение</button> */}
+        {/* Пишем промежуточную стрелочную функцию, чтобы handleclick не вызывался раньше времени */}
+        <button onClick={ () => this.handleClick('Нормально') }>Отправить сообщение</button>
     </div>
 }
 }
+
