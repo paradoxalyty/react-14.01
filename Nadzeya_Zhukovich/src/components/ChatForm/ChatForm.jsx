e34a76a902302a5640128bfdd2909719763715de
Nadzeya <naduva0505@gmail.com>
2020-01-27 23:05:34 +0100

    Added logic for sending message in hooks

--- Nadzeya_Zhukovich/src/components/ChatForm/ChatForm.jsx
+++ Nadzeya_Zhukovich/src/components/ChatForm/ChatForm.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, useRef, useEffect, Component} from "react";
 import PropTypes from 'prop-types';
 
 /**
@@ -9,53 +9,71 @@ import PropTypes from 'prop-types';
  * @param {Function} onSentMessage Message handler
  */
 
-export class ChatForm extends React.Component {
-    constructor(prop) {
-        super(prop);
-        this.state = {
-            name: 'User',
-            content: 'My message'
-        }
-        this.textarea = React.createRef();
-    }
+// export class ChatForm extends React.Component {
+//     constructor(prop) {
+//         super(prop);
+//         this.state = {
+//             name: 'User',
+//             content: 'My message'
+//         }
+//         this.textarea = React.createRef();
+//     }
+//
+//     componentDidMount() {
+//         this.textarea.current.focus();
+//     }
+//
+//     handelInput({currentTarget: {value, name}}){
+//         this.setState(() => ({[name]: value}))
+//     }
+//
+//     handleClick() {
+//         const {name, content} = this.state;
+//         this.props.onSentMessage({name, content});
+//     }
+//
+//     render() {
+//         return (<div>
+//             <input value={this.state.name}
+//                    onChange={this.handelInput.bind(this)}
+//                    name='name'
+//                    type='text'
+//             />
+//             <textarea value={this.state.content}
+//                       onChange={this.handelInput.bind(this)}
+//                       name='content'
+//                       ref={this.textarea}
+//             />
+//             <button onClick={this.handleClick.bind(this)}> Send </button>
+//         </div>);
+//     }
+// }
+export const ChatForm = ({ onSentMessage }) => {
+    const [name, setName] = useState('User');
+    const [content, setContent] = useState('My message');
+    const textarea = useRef();
 
-    componentDidMount() {
-        this.textarea.current.focus();
-    }
+    useEffect(() => {
+        textarea.current.focus();
+    }, []);
 
-    handelInput({currentTarget: {value, name}}){
-        this.setState(() => ({[name]: value}))
-    }
+    const handleClick = () => {
+       onSentMessage({name, content});
+    };
 
-    handleClick() {
-        const {name, content} = this.state;
-        this.props.onSentMessage({name, content});
-    }
-
-    render() {
-        return (<div>
-            <input value={this.state.name}
-                   onChange={this.handelInput.bind(this)}
-                   name='name'
-                   type='text'
-            />
-            <textarea value={this.state.content}
-                      onChange={this.handelInput.bind(this)}
-                      name='content'
-                      ref={this.textarea}
-            />
-            <button onClick={this.handleClick.bind(this)}> Send </button>
-        </div>);
-    }
-}
-// export const ChatForm = ({ message , name, onSentMessage }) =>
-//     (<div>
-//         <input value={name} type={"test"}/>
-//         <textarea value={message}/>
-//         <button> Send </button>
-//     </div>);
-
+    return (<div>
+        <input value={name}
+               type='text'
+               onChange={({currentTarget: {value}}) => setName(value)}
+        />
+        <textarea value={content}
+                  onChange={({currentTarget: {value}}) => setContent(value)}
+                  ref={textarea}
+        />
+        <button onClick={handleClick}> Send </button>
+    </div>);
+};
 
 ChatForm.propTypes = {
     onSentMessage: PropTypes.func.isRequired
-}
+};
