a81cfac5d272386b2a3963e9b6b5aa65668f08e0
roartem <ro.artem@gmail.com>
2020-01-27 11:58:20 +0300

    Переделал мессенджер в соответствии с заданием

--- Artem_Romanov/src/components/Message.jsx
+++ Artem_Romanov/src/components/Message.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 //Отправка сообщение с одиним и тем же текстом по нажатию кнопки
 export default class Message extends React.Component {
-    render() {
-        const {name,content} = this.props;
-        return <div><strong>{name}: </strong>{content}</div>;
-    }
+render() {
+    const {sender,text} = this.props;
+    return <div><strong>{sender}: </strong>{text}</div>;
+}
 }
--- Artem_Romanov/src/components/MessageFiled.jsx
+++ Artem_Romanov/src/components/MessageFiled.jsx
@@ -1,67 +1,34 @@
 import React from 'react';
 import Message from './Message';
 
-//Пример из
-// export default class MessageField extends React.Component {
-//    state = {
-//        //Храним тексты сообщений в массиве, добавляя к нему элементы
-//        messages: ["Привет!", "Как дела?"]
-//    };
-
-// componentDidUpdate() {
-//     if (this.state.messages[this.state.messages.length-1] == "Нормально") {  // Если последний элемент в массиве-"Нормально", то запускаем ответ
-//         setTimeout(() =>
-//         this.setState(
-//             { messages: [ ...this.state.messages, 'Не приставай ко мне, я робот!' ] }),
-//         1500);
-//     }
-// }
-
-//    handleClick = () => {
-//        this.setState({ messages: [ ...this.state.messages, 'Нормально' ] });
-//    };
-
-//    render() {
-//        const messageElements = this.state.messages.map((text, index) => (
-//            <Message key={ index } text={ text } />));
-
-//        return <div>
-//            { messageElements }
-//            <button onClick={ this.handleClick }>Отправить сообщение</button>
-//        </div>
-//    }
-// }
-
 //Кнопка отправки сообщения а так же сами сообщения в качестве дочерних элементов
 export default class MessageField extends React.Component {
-    state = {
-        //Храним тексты сообщений в массиве, добавляя к нему элементы
-        messages: [
-            {name:"Jack", content:"Привет"},
-            {name:"Jack", content:"Как дела?"},
-        ]
-    };
+ //Храним тексты сообщений в массиве, добавляя к нему элементы
+state = {
+    messages: [{ text: "Привет!", sender: 'bot' }, { text: "Как дела?", sender: 'bot' }],
+};
 
-
-    handleClick = () => {
-        this.setState(({messages}) => ({messages: [...messages, {name: 'Jill', content: 'Отлично'}] }));
-    };
-
-    componentDidUpdate() {
-        if (this.state.messages[this.state.messages.length-1].name == "Jill") {// Если последний элемент в массиве равени имени автора, то запускаем ответ
-            setTimeout(() => {
-                this.setState(({messages}) => ({messages: [...messages, {name: 'Robot', content: 'Не лезь, ко мне, я занят'}] }))
-            },1500);
-        }
+componentDidUpdate() {
+    if (this.state.messages[this.state.messages.length - 1].sender === 'me') { // Если последний элемент в массиве равени имени автора, то запускаем ответ
+        setTimeout(() =>
+                this.setState({
+                    messages: [ ...this.state.messages, {text: 'Не лезь, ко мне, я занят', sender: 'bot'} ] }),
+            1000);
     }
+}
+
+handleClick = () => {
+    this.setState({ messages: [ ...this.state.messages, {text: 'Отлично', sender: 'me'} ] });
+};
 
 render() {
-    const MessageElements = this.state.messages.map((text,index) => 
-    (<Message key={index} text {...text}/>));
+    const messageElements = this.state.messages.map((text, index) =>      
+     (<Message key={index} text {...text}/>));
+      
 
-    return <>
-        {MessageElements} 
-        <button onClick={this.handleClick}>Send Message</button>
-    </>
-    }
+    return <div>
+        { messageElements }
+        <button onClick={ this.handleClick }>Отправить сообщение</button>
+    </div>
+}
 }
