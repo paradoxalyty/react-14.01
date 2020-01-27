99ede4a6e7fa9c69e5c5d1e49a12f65eb7d1029d
Maxim <maxpostal@mail.ru>
2020-01-27 22:49:24 +0300

    Remove subfolders

--- Maxim_Vorobiev/Lesson_1/src/js/components/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Button({buttonText}) {
-    return <button id="button" className="button">{buttonText}</button>;
-}
--- Maxim_Vorobiev/Lesson_1/src/js/components/H1.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function H1(props) {
-    return <h1 className="h1">{props.h1Text}</h1>;
-}
