783d76a28b1a21f0404adc9d6acf3a16a372bc24
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-28 12:31:07 +0300

    исправил синтаксис импорта компонентов в Chat.jsx

--- Vladislav Ovinkin/src/components/Chat/Chat.jsx
+++ Vladislav Ovinkin/src/components/Chat/Chat.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import MessageList from '../MessageList/MessageList'
-import ChatForm from '../ChatForm/ChatForm'
+import { MessageList } from '../MessageList/MessageList'
+import { ChatForm } from '../ChatForm/ChatForm'
 
 /**
  *  Компонент объединения формы отправки нового сообщения и области отображения сообщений чата
