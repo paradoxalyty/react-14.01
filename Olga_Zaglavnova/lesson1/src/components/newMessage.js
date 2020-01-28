39090e2c9d1f7414db67d420be3d5b19f8a990b5
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2020-01-28 04:13:16 +0300

    Чищу историю

--- Olga_Zaglavnova/lesson1/src/components/newMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-
-export default class NewMessage extends React.Component {
-    render() {
-        return <form action="#"><input></input>{'\n'}<button></button></form>
-        //return <div>Hello</div>
-    }
-}
