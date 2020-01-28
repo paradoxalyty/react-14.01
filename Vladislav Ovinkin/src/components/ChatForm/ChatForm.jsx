cc27da70c76962c181c3f3de93667ce5df370106
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-28 15:21:13 +0300

    ChatForm.jsx, хуки, начало)

--- Vladislav Ovinkin/src/components/ChatForm/ChatForm.jsx
+++ Vladislav Ovinkin/src/components/ChatForm/ChatForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 
 /**
@@ -8,41 +8,41 @@ import PropTypes from 'prop-types';
  *  @param {string} time - sending message time
  *  @param {Function} onSendMessage - sending new message handler
  */
-export class ChatForm extends React.Component {
-    state = {
-        name: 'User',
-        content: 'My message',
-    }
-    textarea = React.createRef ();
-    componentDidMount () {
-        this.textarea.current.focus ();
-    }
-    handleInput = ({currentTarget: {value, name}}) => {
-        // event.currentTarget.value
-        this.setState(() => ({[name]: value}));
-    }
-    handleClick = () => {
-        const {name, content} = this.state;
-        const date = new Date ();
-        const time = date.toLocaleDateString() + " " + date.toLocaleTimeString();
-        this.props.onSendMessage ({name, content, time});
-    }
-    render () {
-        return (<div>
-            <input name="name" value={ this.state.name } onChange={this.handleInput} type="text" />
-            <textarea name="content" value={ this.state.content} onChange={this.handleInput} ref={this.textarea}/>
-            <button onClick={this.handleClick}>Отправить</button>
-        </div>);
-    }
-}
+// export class ChatForm extends React.Component {
+//     state = {
+//         name: 'User',
+//         content: 'My message',
+//     }
+//     textarea = React.createRef ();
+//     componentDidMount () {
+//         this.textarea.current.focus ();
+//     }
+//     handleInput = ({currentTarget: {value, name}}) => {
+//         // event.currentTarget.value
+//         this.setState(() => ({[name]: value}));
+//     }
+//     handleClick = () => {
+//         const {name, content} = this.state;
+//         const date = new Date ();
+//         const time = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+//         this.props.onSendMessage ({name, content, time});
+//     }
+//     render () {
+//         return (<div>
+//             <input name="name" value={ this.state.name } onChange={this.handleInput} type="text" />
+//             <textarea name="content" value={ this.state.content} onChange={this.handleInput} ref={this.textarea}/>
+//             <button onClick={this.handleClick}>Отправить</button>
+//         </div>);
+//     }
+// }
 
-// export const ChatForm = ({onSendMessage}) => {
-//     return (<div>
-//         <input value={name} type="text" />
-//         <textarea value={ message} />
-//         <button>Отправить</button>
-//     </div>)
-// };
+export const ChatForm = ({onSendMessage}) => {
+    return (<div>
+        <input value={name} type="text" />
+        <textarea value={ message} />
+        <button>Отправить</button>
+    </div>)
+};
 
 ChatForm.propTypes = {
     content: PropTypes.string.isRequired,
