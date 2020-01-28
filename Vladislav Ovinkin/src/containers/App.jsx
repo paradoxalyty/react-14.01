15bf028f45608361f6e9d2da458eeb605c03b7e8
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-28 12:28:24 +0300

    Переделал App.jsx
    пока в нём отрисовывается контейнер ChatContainer

--- Vladislav Ovinkin/src/containers/App.jsx
+++ Vladislav Ovinkin/src/containers/App.jsx
@@ -1,27 +1,10 @@
 import React, { Component } from "react";
-import { Counter } from "../components/Counter"
+import { ChatContainer } from './ChatContainer'
+// import { Counter } from "../components/Counter"
 import PropTypes from "prop-types";
 
 export class App extends Component {
-    state = {
-        isShowCounter: true,
-        count: 0,
-    };
-    static propTypes = {
-        count: PropTypes.string,
-    };
-    handleTrigger = () => {
-        this.setState (({isShowCounter}) => ({isShowCounter: !isShowCounter}));
-    };
-    handleCount = (num) => {
-        this.setState (({count}) => ({ count: count + num }));
-    };
     render () {
-        const {count, isShowCounter} = this.state;
-        return <>
-            <button onClick={ this.handleTrigger }>Show/hide</button>
-            <br />
-            {isShowCounter && <Counter count={count} onCount={this.handleCount}/>}
-        </>
+        return <ChatContainer />
     };
 }
