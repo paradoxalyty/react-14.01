c39802e25120dfbf31403dc10b4c25d4ae44278a
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-28 10:32:46 +0300

    Сделал из MessageList "глупый" компонент
    пока всё поломалось
     - компилируется, но не работает

--- Vladislav Ovinkin/src/components/MessageList/MessageList.jsx
+++ Vladislav Ovinkin/src/components/MessageList/MessageList.jsx
@@ -1,36 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import { Message } from '../Message/Message';
-import { ButtonSend } from '../ButtonSend';
+import PropTypes from 'prop-types';
+// import { ButtonSend } from '../ButtonSend';
 
 const BOT_NAME = "chatBot";
 
-class MessageList extends Component {
-    state = {
-        messages: [
-            {name: "Kolya", content: "Hello!"},
-            {name: "Olya", content: "Hi! How are you?"},
-            {name: "Kolya", content: "I am well"}
-        ]
-    }
-    addMessage = (record) => {
-        const date = new Date ();
-        record.time = date.toLocaleDateString() + " " + date.toLocaleTimeString();
-        this.setState (() => ({messages: this.state.messages.concat(record)}));
-    }
-    componentDidUpdate () {
-        const messages = this.state.messages;
-        const lastName = messages [messages.length - 1].name;
-        if (lastName != BOT_NAME) { // бот сам с собой болтать не будет)
-            const botReply = {name: BOT_NAME, content: lastName + ", робот не спит, он зорко бдит!"};
-            this.addMessage (botReply);
-        }
-    }
-    render () {
-        return <>
-            <ButtonSend onSend={ this.addMessage } />
-            { this.state.messages.map ((message, index) => <Message {...message} key = { index } />)}
-        </>
-    }
+export const MessageList = ({ messages }) => 
+    (messages.map ((message, index) => <Message {...message} key = { index } />));
+
+MessageList.propTypes = {
+    messages: PropTypes.arrayOf(PropTypes.shape(Message.propTypes))
 }
 
-export {MessageList};
+// class MessageList extends Component {
+//     state = {
+//         messages: [
+//             {name: "Kolya", content: "Hello!"},
+//             {name: "Olya", content: "Hi! How are you?"},
+//             {name: "Kolya", content: "I am well"}
+//         ]
+//     }
+//     addMessage = (record) => {
+//         const date = new Date ();
+//         record.time = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+//         this.setState (() => ({messages: this.state.messages.concat(record)}));
+//     }
+//     componentDidUpdate () {
+//         const messages = this.state.messages;
+//         const lastName = messages [messages.length - 1].name;
+//         if (lastName != BOT_NAME) { // бот сам с собой болтать не будет)
+//             const botReply = {name: BOT_NAME, content: lastName + ", робот не спит, он зорко бдит!"};
+//             this.addMessage (botReply);
+//         }
+//     }
+//     render () {
+//         return <>
+//             <ButtonSend onSend={ this.addMessage } />
+//             { this.state.messages.map ((message, index) => <Message {...message} key = { index } />)}
+//         </>
+//     }
+// }
+
+// export {MessageList};
