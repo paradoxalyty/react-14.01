d21c72ffd018f643b34de08b2a50597a55699a3b
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-30 00:11:54 +0300

    теперь не просто список сообщений,
    а список чатов со своими списками сообщений в каждом.
    И выключенный на время робот.

--- Vladislav Ovinkin/src/containers/ChatContainer.jsx
+++ Vladislav Ovinkin/src/containers/ChatContainer.jsx
@@ -5,22 +5,38 @@ const BOT_NAME = "chatBot";
 
 export class ChatContainer extends Component {
     state = {
-        messages: [
-            {name: "Kolya", content: "Hello!"},
-            {name: "Olya", content: "Hi! How are you?"},
-            {name: "Kolya", content: "I am well"},
-        ],
+        chats : {
+            1: {
+                name: 'Anna',
+                messages: [
+                    {name: "Robot", content: "Hello!"},
+                    {name: "Anna", content: "Hi! How are you?"},
+                    {name: "Robot", content: "I am well!"},
+                ],
+            },
+            2: {
+                name: 'Elena',
+                messages: [
+                    {name: "Robot", content: "Hello!"},
+                    {name: "Elena", content: "Hi! It's interesting to talking with robot for me)"},
+                ],
+            },
+            3: {
+                name: 'Olga',
+                messages: [],
+            },
+        }
     }
     timer = null;
 
     componentDidUpdate () {
-        clearTimeout (this.timer);
+        // clearTimeout (this.timer);
         
-        const lastMessage = this.state.messages[this.state.messages.length - 1];
+        // const lastMessage = this.state.messages[this.state.messages.length - 1];
         
-        if (lastMessage.name !== BOT_NAME) {
-            this.timer = setTimeout(() => this.handleSendMessage ({name: BOT_NAME, content: `Hi, ${lastMessage.name}, I'm a robot!`}), 2000);
-        }
+        // if (lastMessage.name !== BOT_NAME) {
+        //     this.timer = setTimeout(() => this.handleSendMessage ({name: BOT_NAME, content: `Hi, ${lastMessage.name}, I'm a robot!`}), 2000);
+        // }
     }
     
     componentWillUnmount () {
@@ -35,8 +51,12 @@ export class ChatContainer extends Component {
     }
 
     render () {
-        const {messages} = this.state;
-
-        return <Chat {...{messages, onSendMessage: this.handleSendMessage}} />
+        const {chats} = this.state;
+        const {id} = this.props.match.params;
+        if (id) {
+            return <Chat {...{messages: chats[id].messages, onSendMessage: this.handleSendMessage}} />
+        } else {
+            return "Ошибка: необходимо выбрать номер чата.";
+        }
     }
 }
