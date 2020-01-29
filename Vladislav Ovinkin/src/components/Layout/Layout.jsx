6adcff77ad7e3b6bff54e63164e86a0fcd2010cc
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-30 00:10:06 +0300

    Сделано по мотивам урока
    1. Подключить ​ BrowserRouter​ (из ​ react-router-dom​ ).
    2. Создать верхний компонент ​ Router​ со ​ Switch​ и ​ Route​ ’ами.

--- Vladislav Ovinkin/src/components/Layout/Layout.jsx
+++ Vladislav Ovinkin/src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import { ChatList } from '../ChatList/ChatList';
 import { ChatContainer } from '../../containers/ChatContainer'
@@ -6,10 +7,24 @@ import './Layout.css'
 
 export const Layout = () => {
     return ( <div className="layoutHeader">
-        <Header />
-        <div className="layoutChatList">
-            <ChatList className="chatList" />
-            <ChatContainer className="chatContainer" />
-        </div>
-    </div>);
+                <Header />
+                <div className="layoutChatList">
+                    <BrowserRouter>
+                        <ChatList />
+                        <Switch>
+                            <Route path="/chats/" exact component={ChatContainer} />
+                            <Route path="/chats/:id" exact component={ChatContainer} />
+                            <Route path="/about">
+                                It's about
+                            </Route>
+                            <Route path="/home">
+                                It's home
+                            </Route>
+                            <Route path="/">
+                                It's 404
+                            </Route>
+                        </Switch>
+                    </BrowserRouter>
+                </div>
+            </div>);
 }
