63b8794093b3f8f8a7def0f5c4a0a6249861a611
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-30 00:12:22 +0300

    небольшие настройки

--- Vladislav Ovinkin/src/containers/App.jsx
+++ Vladislav Ovinkin/src/containers/App.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-// import { ChatContainer } from './ChatContainer';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component, useState } from "react";
 import { Layout } from '../components/Layout/Layout';
 
 export class App extends Component {
     render () {
-        return <Layout />
+        return (
+            <Layout />
+        );
     };
 }
