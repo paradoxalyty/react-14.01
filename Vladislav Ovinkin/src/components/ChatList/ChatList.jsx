a71075af29ea3c0c3970d999e30c253c2116aca2
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2020-01-30 00:08:45 +0300

    Подключен компонент List из material-ui
    Настроен список чатов, ссылки на них (на следующем шаге думаю генерировать этот список автоматически на основе списка чатов из state, для этого state надо поднять повыше.

--- Vladislav Ovinkin/src/components/ChatList/ChatList.jsx
+++ Vladislav Ovinkin/src/components/ChatList/ChatList.jsx
@@ -6,11 +6,12 @@ import './ChatList.css'
 
 
 export const ChatList = () => {
-    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    // const [selectedIndex, setSelectedIndex] = React.useState(0); // подсветка в списке чата 1
+    const [selectedIndex, setSelectedIndex] = React.useState(); // нет подсветки
 
     const handleListItemClick = (event, index) => {
         setSelectedIndex(index);
-    };
+    }
 
     function ListItemLink(props) {
         return <ListItem button component="a" {...props} />;
@@ -21,19 +22,19 @@ export const ChatList = () => {
             <ListItemLink 
             selected={selectedIndex === 0}
             onClick={event => handleListItemClick(event, 0)}
-            href="/Anna">
+            href="/chats/1">
                 <ListItemText primary="Anna" />
             </ListItemLink>
             <ListItemLink 
             selected={selectedIndex === 1}
             onClick={event => handleListItemClick(event, 1)}
-            href="/Elena">
+            href="/chats/2">
                 <ListItemText primary="Elena" />
             </ListItemLink>
             <ListItemLink
             selected={selectedIndex === 2}
             onClick={event => handleListItemClick(event, 2)}
-            href="/Olga">
+            href="/chats/3">
                 <ListItemText primary="Olga" />
             </ListItemLink>
         </List>
