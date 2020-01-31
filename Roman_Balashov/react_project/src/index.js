cfa4207ff43090cb5772a4942818398e83f72aa5
inverse231 <3xinverse@gmail.com>
2020-01-31 14:35:53 +0300

    template

--- Roman_Balashov/react_project/src/index.js
+++ Roman_Balashov/react_project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style';
+import './style.css';
 import { Layout } from './components/Layout/Layout';
 
 ReactDOM.render(<Layout />,
