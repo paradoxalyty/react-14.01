979a2c9187d18176b04335ac12b7b14d680618f0
Alexeybobkov47 <Alexeybobkov47@gmail.com>
2020-02-04 22:59:58 +0400

    Мелкие правка

--- Alexey_Bobkov/1-src/containers/ChatContainer.jsx
+++ Alexey_Bobkov/1-src/containers/ChatContainer.jsx
@@ -1,75 +1,75 @@
 import React, { Component } from 'react';
 //import { Layout } from '../components/Layout/Layout.jsx';
-import {addMessage, loadChats} from '../store/chatAction.js'
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { addMessage, loadChats } from '../store/chatAction.js'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 
 
 class ChatContainer extends Component {
 
-//     state ={
-//         chats: {
-//             1:{
-//                 id:1,
-//                 name: 'Chat 1',
-//                 messages: [
-//                                 { name: 'Name1', content: 'Text1' }  
-//                             ],
-//                 },
-//             2: {
-//                 id:2,
-//                 name: 'Chat 2',
-//                 messages: [
-//                                 { name: 'Name2', content: 'Text2' }
-                                
-//                 ]
-//                 },
-//             3: {
-//                 id:3,
-//                 name: 'Chat 3',
-//                 messages: [
-                                
-//                                 { name: 'Name3', content: 'Text3' }
-                                
-//                 ]
-//                 },
-//             4: {
-//                 id:4,
-//                 name: 'Chat 4',
-//                 messages: [
-            
-//                                 { name: 'Name4', content: 'Text4' }
-//                 ]
-//                 },
-//             5: {
-//                 id:5,
-//                 name: 'Chat 5',
-//                 messages: [
-                                
-//                                 { name: 'Name5', content: 'Text5' }
-//                 ]
-//                 },
+    //     state ={
+    //         chats: {
+    //             1:{
+    //                 id:1,
+    //                 name: 'Chat 1',
+    //                 messages: [
+    //                                 { name: 'Name1', content: 'Text1' }  
+    //                             ],
+    //                 },
+    //             2: {
+    //                 id:2,
+    //                 name: 'Chat 2',
+    //                 messages: [
+    //                                 { name: 'Name2', content: 'Text2' }
 
-//             }
-//         }
+    //                 ]
+    //                 },
+    //             3: {
+    //                 id:3,
+    //                 name: 'Chat 3',
+    //                 messages: [
+
+    //                                 { name: 'Name3', content: 'Text3' }
+
+    //                 ]
+    //                 },
+    //             4: {
+    //                 id:4,
+    //                 name: 'Chat 4',
+    //                 messages: [
+
+    //                                 { name: 'Name4', content: 'Text4' }
+    //                 ]
+    //                 },
+    //             5: {
+    //                 id:5,
+    //                 name: 'Chat 5',
+    //                 messages: [
+
+    //                                 { name: 'Name5', content: 'Text5' }
+    //                 ]
+    //                 },
+
+    //             }
+    //         }
 
 
 
     handleSendMessage = (id) => (message) => {
         this.props.addMessage(id, message.name, message.content);
-//         this.setState((state) => ({chats: {...state.chats,
-//             [id]: {name: state.chats[id].name,
-//         messages: [...state.chats[id].messages, message,]},
-// }
-    // }
-    // ));
-}
-    componentDidMount(){
+        //         this.setState((state) => ({chats: {...state.chats,
+        //             [id]: {name: state.chats[id].name,
+        //         messages: [...state.chats[id].messages, message,]},
+        // }
+        // }
+        // ));
+    }
+    componentDidMount() {
         this.props.loadChats();
     }
     // componentDidUpdate(){
-        
+
     //    const {chats} = this.state;
     //     const {id} = this.props.match.params;
     //     if(id && chats[id]){
@@ -80,32 +80,32 @@ class ChatContainer extends Component {
     //         } else {
     //             var classrobot = document.getElementsByClassName('Message')[document.getElementsByClassName('Message').length-1];
     //             classrobot.className =  classrobot.className + ' Robot';
-                
+
     //             }
-        
+
     //              } 
     //     }
 
-     render () {
-        
-        const {chats} = this.props;
-        const {id} = this.props.match.params;
-        if(id && chats[id]){
-             return <Layout {...{messages: chats[id].messages, onSendMessage: this.handleSendMessage(id)}} />
+
+    render() {
+        const { chats } = this.props;
+        const { id } = this.props.match.params;
+        if (id && chats[id]) {
+            return <Layout {...{ messages: chats[id].messages, onSendMessage: this.handleSendMessage(id) }} />
         }
 
-        
-      }
+
+    }
 };
 
 
-const mapStateToProps = ({chatReducer}) => ({
-       chats: chatReducer.chats
+const mapStateToProps = ({ chatReducer }) => ({
+    chats: chatReducer.chats
 });
 
 
-const mapDispatchToProps = ({}) => {
-    return bindActionCreators( {loadChats, addMessage}, dispatch, )
+const mapDispatchToProps = ({ }) => {
+    return bindActionCreators({ loadChats, addMessage }, dispatch)
 };
 
-export default connect(mapStateToProps, mapDispatchToProps,)(ChatContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ChatContainer);
