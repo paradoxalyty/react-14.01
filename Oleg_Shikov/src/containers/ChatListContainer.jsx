db9a378a9852e725d9d78c17c33661d0e85daa03
Oleg Shikov <ol.shikov@yandex.ru>
2020-02-11 20:55:54 +0300

    Lesson 7

--- Oleg_Shikov/src/containers/ChatListContainer.jsx
+++ Oleg_Shikov/src/containers/ChatListContainer.jsx
@@ -6,12 +6,13 @@ import { addChat } from '../store/chatAction';
 const mapStateToProps = (store) => {
     const chats = Object.keys(store.chatReducer.chats).map((id) => ({
             id,
-            name: store.chatReducer.chats[id].name
+            name: store.chatReducer.chats[id].name,
+            unread: store.chatReducer.chats[id].unread
         }));
     return {
         chats
     }
 }
-const mapDispatchToProps = dispatch => bindActionCreators({addChat}, dispatch);
+const mapDispatchToProps = dispatch =>  bindActionCreators({addChat}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatList)
