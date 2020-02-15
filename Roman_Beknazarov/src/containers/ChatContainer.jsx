c868aa430188c9f47cdb309ad69368a6e9ab108c
paradoxalyty <paradoxalyty@gmail.com>
2020-02-15 13:23:45 +0100

    conditional (ternary) operator, clearTimeout

--- Roman_Beknazarov/src/containers/ChatContainer.jsx
+++ Roman_Beknazarov/src/containers/ChatContainer.jsx
@@ -5,7 +5,20 @@ import {addMessage} from '../store/chatAction';
 
 const mapStateToProps = ({chatReducer}, {match}) => {
     const id = match.params.id;
+
+         // let result;
+         // if (id) {
+         //     if (chatReducer.chats[id]) {
+         //         result = chatReducer.chats[id].messages;
+         //     } else {
+         //         result = null;
+         //     }
+         // } else {
+         //     result = null;
+         // }
+
     return {
+        // messages: result
         messages: id ? chatReducer.chats[id] ? chatReducer.chats[id].messages : null : null
     };
 };
