7fb782b1d3788149089a23704e7b3a3c4c8cfff5
paradoxalyty <paradoxalyty@gmail.com>
2020-02-15 12:46:30 +0100

    redux-persist

--- Roman_Beknazarov/src/containers/App.jsx
+++ Roman_Beknazarov/src/containers/App.jsx
@@ -3,16 +3,19 @@ import React, {Component} from 'react';
 import {Layout} from '../components/Layout/Layout';
 import {initStore} from '../store/store';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import {loadChats} from '../store/chatAction';
 
-const store = initStore();
+const {store, persistor} = initStore();
 store.dispatch(loadChats());
 
 export class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Layout/>
+                <PersistGate loading={null} persistor={persistor}>
+                    <Layout/>
+                </PersistGate>
             </Provider>
         );
     }
--- Roman_Beknazarov/src/store/store.js
+++ Roman_Beknazarov/src/store/store.js
@@ -5,6 +5,16 @@ import chatMiddleware from './chatMiddleware';
 import {createLogger} from 'redux-logger/src';
 import {createBrowserHistory} from 'history';
 import {routerMiddleware, connectRouter} from 'connected-react-router';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import {persistStore, persistReducer} from 'redux-persist';
+
+const persistConfig = {
+    key: 'messanger',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whitelist: ['messageReducer', 'chatReducer'],
+};
 
 export const history = createBrowserHistory();
 
@@ -17,9 +27,13 @@ const logger = createLogger();
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : () => {};
 
 export const initStore = (preloadedState = {}) => {
-    return createStore(
-        reducer,
+    const store = createStore(
+        persistReducer(persistConfig, reducer),
         preloadedState,
         compose(applyMiddleware(routerMiddleware(history), logger, botMiddleware, chatMiddleware), devTools),
     );
+
+    const persistor = persistStore(store);
+
+    return {store, persistor};
 };
