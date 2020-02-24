b4827de45c7dab19698ccaf3c07a839c0ea348c7
inverse231 <3xinverse@gmail.com>
2020-02-24 19:02:24 +0300

    added manifest and service worker

---
+++ Roman_Balashov/react_project/service-worker.js
@@ -0,0 +1,50 @@
+let doCache = true;
+
+const CACHE_NAME = "react-chat-cache";
+
+self.addEventListener("activate", event => {
+    const cacheWhiteList = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys()
+            .then(keyList =>
+                Promise.all(keyList.map(key => {
+                    if (!cacheWhiteList.includes(key)) {
+                        console.log("Deleting cache:" + key);
+                        return caches.delete(key);
+                    }
+                }))
+            )
+    );
+});
+
+self.addEventListener('install', function (event) {
+    if (doCache) {
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(function (cache) {
+                    fetch('manifest/manifest.json')
+                        .then(response => {
+                            response.json()
+                        })
+                        .then(assets => {
+                            const urlsToCache = [
+                                '',
+                                '/chat/*',
+                            ];
+                            cache.addAll(urlsToCache);
+                            console.log('cached');
+                        })
+                })
+        );
+    }
+});
+
+self.addEventListener('fetch', function (event) {
+    if (doCache) {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
